7e7ecdc57f5a69e7c6666027b6ae3a7d80a82645
datigrov <denistigrov@icloud.com>
2024-04-14 17:39:03 +0300

    first step

---
+++ src/main/java/ru/hogwarts/school/DemoApiSwaggerPostmanApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiSwaggerPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiSwaggerPostmanApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+
+public class FacultyService {
+    private final HashMap<Long, Faculty> facultyServiceInfo = new HashMap<>();
+
+}
