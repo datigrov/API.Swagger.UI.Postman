574392a95d6fe95c68ffe041cca1a3d635fc134f
datigrov <denistigrov@icloud.com>
2024-06-09 16:58:29 +0300

    4.2 complete

--- scripts422.sql
+++ scripts422.sql
@@ -1,16 +1,14 @@
 create table car(
-    car_id integer primary key,
+    id integer primary key,
     model varchar,
     brand varchar,
     price integer
 );
 
 create table human(
-    human_id integer primary key,
-    name varchar,
-    age_human integer,
-    human_license boolean
-);
-create table human_and_car(
-    human_id Integer references car(car_id)
+    id serial primary key,
+    name text not null,
+    age integer not null,
+    driver_license boolean,
+    car_id integer not null references car(id)
 );
