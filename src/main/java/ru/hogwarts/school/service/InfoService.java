9490bce4a5f3439d6c1df3a5488bc85a4309ec18
datigrov <denistigrov@icloud.com>
2024-06-23 20:24:06 +0300

    4.5 Parallel Streams #3

--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.interfaceUniversity.InfoInterface;
 
+import java.util.stream.Stream;
+
 @Service
 public class InfoService implements InfoInterface {
 
@@ -23,7 +25,10 @@ public class InfoService implements InfoInterface {
     }
 
     public Integer getWholeSum() {
-        // int sum = Stream.iterate(1, a -> a +1) .limit(1_000_000) .reduce(0, (a, b) -> a + b );
+        int sum = Stream.iterate(1, a -> a + 1)
+                .limit(1_000_000)
+                .reduce(0, (a, b) -> a + b);
+        return sum;
 
     }
 
