717a9b42366cf90d0d6c90b3f07428ce28c51d4b
datigrov <denistigrov@icloud.com>
2024-06-27 23:26:38 +0300

    4.5 Parallel Streams #4

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,9 +1,14 @@
 package ru.hogwarts.school.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hogwarts.school.service.InfoService;
+
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @RestController
 public class InfoController {
@@ -21,6 +26,20 @@ public class InfoController {
 
     @GetMapping("/wholeSum")
     public Integer wholeSum() {
-        return infoService.getWholeSum();
+        Logger logger = (Logger) LoggerFactory.getLogger(InfoService.class);
+        int sum;
+        long slowTime = System.currentTimeMillis();
+        sum = Stream.iterate(1, a -> a + 1)
+                .limit(1_000_000)
+                .reduce(0, (a, b) -> a + b);
+        logger.info("Slow time is: " + slowTime);
+
+        long fasterTime = System.currentTimeMillis();
+        sum = IntStream.iterate(1, a -> a + 1)
+                .parallel()
+                .limit(1_000_000)
+                .reduce(1, Integer::sum);
+        logger.info("better time is: " + (System.currentTimeMillis() - fasterTime));
+        return sum;
     }
 }
--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -26,23 +26,5 @@ public class InfoService implements InfoInterface {
         return serverProperties.getPort();
     }
 
-    public Integer getWholeSum() {
-        Logger logger = (Logger) LoggerFactory.getLogger(InfoService.class);
-        int sum;
-        long slowTime = System.currentTimeMillis();
-        sum = Stream.iterate(1, a -> a + 1)
-                .limit(1_000_000)
-                .reduce(0, (a, b) -> a + b);
-        logger.info("Slow time is: " + slowTime);
-
-        long fasterTime = System.currentTimeMillis();
-        sum = IntStream.iterate(1, a -> a + 1)
-                .parallel()
-                .limit(1_000_000)
-                .reduce(1, Integer::sum);
-        logger.info("better time is: " + (System.currentTimeMillis() - fasterTime));
-        return sum;
-
-    }
 
 }
